ca4bd52e5f8978ca53de2225fb3d81820e0bec11
Ricardox29 <147215696+Ricardox29@users.noreply.github.com>
2024-06-19 19:30:42 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Wellcome to my personal project 
+
+
+This is a second project practice in react ,I hope you like this simple project, gif me a star thanks.
---
+++ src/components/Navigartion/Navigation.jsx
@@ -0,0 +1,22 @@
+import styles from './Navigation.module.css';
+
+const Navigation = () => {
+
+    return (
+        <nav className="container">
+            <div className="image-nav">
+                <img src=".\images\panda.png" alt="Imagen logo de la pagina" />
+            </div>
+
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Ubication</a></li>
+                <li><a href="">Contact us</a></li>
+            </ul>
+
+        </nav>
+    );
+
+};
+
+export default Navigation;
